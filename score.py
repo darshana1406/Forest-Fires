71e177e48d9ed4c4979ef1b61ccda4c22d9353c8
lady-ada <45360690+lady-ada@users.noreply.github.com>
2021-02-04 12:15:37 +0530

    Add files via upload

--- score.py
+++ score.py
@@ -1,17 +1,18 @@
 import json
-import numpy as np
+import pandas as pd
 import os
-from sklearn.externals import joblib
+import joblib
+import azureml.train.automl
 
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'outputs/model.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
     model = joblib.load(model_path)
 
 def run(data):
     try:
-        data = np.array(json.loads(data))
+        data = pd.DataFrame(json.loads(data)['data'])
         result = model.predict(data)
         return result.tolist()
     except Exception as e:
